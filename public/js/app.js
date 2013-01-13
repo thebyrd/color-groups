8faaf0eec036cccf6a08b7674ce3ad93209ed2e3
David Byrd <david@byrdhou.se>
2013-01-13 12:34:21 -0800

    spaced everything out

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,17 @@
-var App = {
-  createMatrix: function (n, m) {
+
+var Help = {
+  createMatrix: function (n, m) { //From the Problem Handout
     var matrix = [];
     for (var i = 0; i < n; i++) {
       matrix[i] = [];
       for (var j = 0; j < m; j++) {
         matrix[i].push(Math.round(Math.random())); 
       }
-    }
+    } 
     return matrix; 
+  },
+  randColor: function () { //From Paul Irish's Blog
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
   }
 };
 
@@ -22,12 +26,10 @@ function Grid (height, width) {
   this.width = width;
   this.cellsByID = {};
   this.neighborsByCellID = {};
-  var matrix = App.createMatrix(this.height, this.width);
+  var matrix = Help.createMatrix(this.height, this.width);
   this.populateMaps(matrix);
   this.setColors();
 }
-
-var count = 0;
 
 Grid.prototype.populateMaps = function (binaryMatrix) {
   var m = binaryMatrix;
@@ -36,7 +38,7 @@ Grid.prototype.populateMaps = function (binaryMatrix) {
       var hasColor = m[y][x];
       if (hasColor) {
 
-        /* get neighbors */
+        // get neighbors (left, right, bottom, top)
         var neighbors = [];
 
         if (m[y-1]    !== undefined && m[y-1][x]) neighbors.push( new Cell(x, y-1) );
@@ -44,13 +46,14 @@ Grid.prototype.populateMaps = function (binaryMatrix) {
         if (m[y][x-1] !== undefined && m[y][x-1]) neighbors.push( new Cell(x-1, y) );
         if (m[y][x+1] !== undefined && m[y][x+1]) neighbors.push( new Cell(x+1, y) );
 
+        // if the cell already exists in the cellsByID map use that cell for storing neighbors
         var cell = new Cell(x,y);
-
         this.cellsByID[cell.id] ? 
           cell = this.cellsByID[cell.id] : this.cellsByID[cell.id] = cell
 
         this.neighborsByCellID[cell.id] = neighbors;
 
+        //add neighbors to cellsByID
         for (var i = 0; i < neighbors.length; i++) this.cellsByID[neighbors[i].id] = neighbors[i];
         
       }
@@ -63,22 +66,26 @@ Grid.prototype.setColors = function () {
   for (var id in this.cellsByID) {
     var cell = this.cellsByID[id];
     if (cell.color === undefined) {
-
-      color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      color = Help.randColor();
       cell.color = color;
 
-      var queue = [];
-      queue.push(cell);
-      while (queue.length > 0) {
-        var next = queue.shift();
+      var q = [];
+      q.push(cell);
+
+      while (q.length > 0) {
+        var next = q.shift();
         var neighbors = this.neighborsByCellID[next.id];
+
         for (var i = 0; i < neighbors.length; i++) {
           var n = neighbors[i];
+          
           if (n.color === undefined) {
             n.color = color;
-            queue.push(n);
+            q.push(n);
           }
+
         }
+
       }
     }
   }
@@ -86,20 +93,26 @@ Grid.prototype.setColors = function () {
 
 Grid.prototype.render = function () {
 
-  var c1 = document.getElementsByClassName('grayscale')[0];
-  var cntxt1 = c1.getContext('2d');
-  for (var id1 in this.cellsByID) {
-    var cell1 = this.cellsByID[id1];
-    cntxt1.fillStyle = 'black';
-    cntxt1.fillRect(20 * cell1.x, 20 * cell1.y, 20, 20);
+  var grayCnvs = document.getElementsByClassName('grayscale')[0];
+  var grayCntxt = grayCnvs.getContext('2d');
+
+  for (var grayID in this.cellsByID) {
+    var grayCell = this.cellsByID[grayID];
+
+    grayCntxt.fillStyle = 'black';
+    grayCntxt.fillRect(20 * grayCell.x, 20 * grayCell.y, 20, 20);
+
   }
 
-  var c2 = document.getElementsByClassName('colorful')[0];
-  var cntxt2 = c2.getContext('2d');
-  for (var id2 in this.cellsByID) {
-    var cell2 = this.cellsByID[id2];
-    cntxt2.fillStyle = cell2.color || '#fff';
-    cntxt2.fillRect(20 * cell2.x, 20 * cell2.y, 20, 20);
+  var colorCnvs = document.getElementsByClassName('colorful')[0];
+  var colorCntxt = colorCnvs.getContext('2d');
+
+  for (var colorID in this.cellsByID) {
+    var coloredCell = this.cellsByID[colorID];
+
+    colorCntxt.fillStyle = coloredCell.color || '#fff';
+    colorCntxt.fillRect(20 * coloredCell.x, 20 * coloredCell.y, 20, 20);
+
   }
 };
 
